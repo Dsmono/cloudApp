8d87fb1caee16646a5ade195d2dc8586f8bade73
Dsmono <64215915+Dsmono@users.noreply.github.com>
2021-11-13 16:08:39 -0500

    main

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $a=$_GET['DEPARTAMENTO'];
 $b=$_GET['EVENTO'];
 
 $client = new MongoDB\Client('mongodb+srv://jaun:123456a@cluster0.0jfro.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-$tb=$client->emergencia->danos
+$tb=$client->emergencia->danos;
 $filter= ['$and'=>
             [
                 ['DEPARTAMENTO'=>['$eq'=>'ANTIOQUIA']],
