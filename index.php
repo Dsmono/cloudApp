dc524e1393d21f51a47e84d8f99c05a683b2ed68
Dsmono <64215915+Dsmono@users.noreply.github.com>
2021-11-13 18:22:49 -0500

    m

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 
 require_once __DIR__. '/vendor/autoload.php';
-
-//$a=$_GET['DEPARTAMENTO'];
-//$b=$_GET['EVENTO'];
+$a=$_GET['DEPARTAMENTO'];
+$b=$_GET['EVENTO'];
 
 $client = new MongoDB\Client('mongodb+srv://jaun:123456a@cluster0.0jfro.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 $tb=$client->emergencia->dano;
