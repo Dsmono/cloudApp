29584798e83ce57a1386a8dd21127850161803ce
Dsmono <64215915+Dsmono@users.noreply.github.com>
2021-11-13 16:02:24 -0500

    main

--- index.php
+++ index.php
@@ -2,27 +2,9 @@
 
 require_once __DIR__. '/vendor/autoload.php';
 
-$a=$_GET['anno'];
-$b=$_GET['represa'];
+$a=$_GET['DEPARTAMENTO'];
+$b=$_GET['EVENTO'];
 
-/*$client = new MongoDB\Client('mongodb+srv://jaun:123456a@cluster0.0jfro.mongodb.net/ServiciosMul?retryWrites=true&w=majority');
-
-$tb = $client->ServiciosMul->puntaje;
-
-$registro = array(
-    "iduser"=>"1","puntaje"=>"22"
-);
-
-$resultado=$tb->insertOne($registro);
-
-
-    $client = new MongoDB\Client('mongodb+srv://jaun:123456a@cluster0.0jfro.mongodb.net/ServiciosMul?retryWrites=true&w=majority');
-
-$db = $client->test;
-
-
-
-echo json_encode($registro);*/
 $client = new MongoDB\Client('mongodb+srv://jaun:123456a@cluster0.0jfro.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 $tb=$client->emergencia->danos
 $filter= ['$and'=>
