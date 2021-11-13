0630ba7664616d9e3cd62fc5e6ae3ed6a507c052
Dsmono <64215915+Dsmono@users.noreply.github.com>
2021-11-13 18:34:42 -0500

    M

--- index.php
+++ index.php
@@ -6,13 +6,7 @@ $b=$_GET['EVENTO'];
 
 $client = new MongoDB\Client('mongodb+srv://jaun:123456a@cluster0.0jfro.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 $tb=$client->emergencia->dano;
-$filter= ['$and'=>
-            [
-                ['DEPARTAMENTO'=>['$eq'=>'ANTIOQUIA']],
-                ['EVENTO'=>['$eq'=>'INCENDIO ESTRUCTURAL']],
-                
-            ]
-            ];
+
 $query = new MongoDB\Driver\Query($filter);
 $rows = $tb->find($filter);
 $datos= iterator_to_array($rows);
