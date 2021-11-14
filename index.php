1a434c6cf8fafc84f3d78fb5644f961ba64c53fe
Dsmono <64215915+Dsmono@users.noreply.github.com>
2021-11-13 20:01:00 -0500

    m

--- index.php
+++ index.php
@@ -3,14 +3,13 @@
 require_once __DIR__. '/vendor/autoload.php';
 $a=$_GET['DEPARTAMENTO'];
 $b=$_GET['MUNICIPIO'];
-$b=$_GET['EVENTO'];
 
 $client = new MongoDB\Client('mongodb+srv://jaun:123456a@cluster0.0jfro.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 $tb=$client->emergencia->dano;
 $filter= ['$and'=>
             [
-                ['DEPARTAMENTO'=>['$eq'=>$a]],
-                ['EVENTO'=>['$eq'=>$b]],
+               
+                ['MUNICIPIO'=>['$eq'=>$b]],
                 ['SUBSIDIO DE ARRIENDO'=>['$eq'=>'0']],
                 
             ]
